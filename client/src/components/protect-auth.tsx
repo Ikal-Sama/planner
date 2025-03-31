cc048598fb7ede0383c467a3248ed8b02e284e0d
IkalSama <joestarjosuke28@gmail.com>
2025-03-31 08:49:23 +0800

    fix error

--- client/src/components/protect-auth.tsx
+++ client/src/components/protect-auth.tsx
@@ -1,15 +1,10 @@
 import { authQueryOptions } from "@/queryOptions/authQueryOptions";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 export default function ProtectedAuth() {
   const location = useLocation();
-  const queryClient = useQueryClient();
-  const { data, isError } = useQuery(authQueryOptions());
-  if (isError) {
-    queryClient.removeQueries({ queryKey: ["auth"] });
-    return <Navigate to='/signin' state={{ from: location }} replace />;
-  }
+  const { data } = useQuery(authQueryOptions());
 
   if (data) {
     return <Navigate to='/dashboard' state={{ from: location }} replace />;
